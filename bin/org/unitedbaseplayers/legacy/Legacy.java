49b6f748fd8e2c166e2184cc25cef0a5af70c9df
Benjamin Kantor <elwoodini@hotmail.com>
2013-03-27 21:02:30 -0500

    Created Legacy.java

--- bin/org/unitedbaseplayers/legacy/Legacy.java
+++ bin/org/unitedbaseplayers/legacy/Legacy.java
@@ -1,1 +1,51 @@
-blah blah
+package org.unitedbaseplayers.legacy;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Legacy extends JavaPlugin {
+    @Override
+    public void onEnable(){
+        // TODO Insert logic to be performed when the plugin is enabled
+      getLogger().info("onEnable has been invoked!");
+    }
+    
+    @Override
+    public void onDisable() {
+        // TODO Insert logic to be performed when the plugin is disabled
+    	getLogger().info("onDisable has been invoked!");
+    }
+    
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
+    	if (cmd.getName().equalsIgnoreCase("leap"))
+    	{
+    		if (!(sender instanceof Player)) 
+    		{
+    			sender.sendMessage("This command can only be run by a player.");
+            	return false;
+    		}
+    		else 
+    		{
+    			sender.sendMessage("Leap now " + sender.getName() + "!");
+            	return true;
+    		}
+    	}
+    	else if (cmd.getName().equalsIgnoreCase("fart"))
+    	{
+    		if (!(sender instanceof Player)) 
+    		{
+    			sender.sendMessage("This command can only be run by a player.");
+            	return false;
+    		}
+    		else 
+    		{
+    			sender.sendMessage("Excuse me!");
+    			// playEffect()
+            	return true;
+    		}
+    	}
+		return false;
+    } 
+}
